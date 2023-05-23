8d0c2984cad92b1db72618a08df23326c76a4f04
Aleksandr <voitenko_1986@mail.ru>
2023-05-23 16:28:44 +0700

    dsbgzs

--- 01/src/main.tf
+++ 01/src/main.tf
@@ -28,7 +28,7 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
-  #name  = "example_${random_password.random_string_fake.resuld}"
+  #name  = "example_${random_password.random_string.result}"
   name = "Heloo_World"
   ports {
     internal = 80
