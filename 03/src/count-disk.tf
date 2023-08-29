63ae5157a6e9bc89eca6bf2ef4208e214b88e7ae
VoitenkoAN <110226611+VoitenkoAN@users.noreply.github.com>
2023-08-29 16:30:35 +0700

    Update count-disk.tf
    security_group_ids

--- 03/src/count-disk.tf
+++ 03/src/count-disk.tf
@@ -30,6 +30,7 @@ resource "yandex_compute_instance" "wm-disk" {
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
     nat       = var.vm_web_platform_network_interface_nat
+    security_group_ids = [yandex_vpc_security_group.example.id]
   }
 
   metadata = {
